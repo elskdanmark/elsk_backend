d8d25fb6a5252960dd6f7c47dcab9eb0535ae842
Zeohye <Nilshenning@live.dk>
2017-03-26 17:07:04 +0200

    Added tests to UserController

--- src/Elsk/CoreBundle/Tests/Controller/UserControllerTest.php
+++ src/Elsk/CoreBundle/Tests/Controller/UserControllerTest.php
@@ -13,4 +13,28 @@ class UserControllerTest extends WebTestCase
         $crawler = $client->request('POST', '/register/recipient', ['email' => 'test@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
         $this->assertEquals($crawler->text(),"User created");
     }
+    
+    public function testRegisterVolunteer()
+    {
+    	$client = static::createClient();
+    
+    	$crawler = $client->request('POST', '/register/volunteer', ['email' => 'test2@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals($crawler->text(),"User created");
+    }
+    
+    public function testRegisterLocalAdmin()
+    {
+    	$client = static::createClient();
+    
+    	$crawler = $client->request('POST', '/admin/register/localAdmin', ['email' => 'test3@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals($crawler->text(),"User created");
+    }
+    
+    public function testRegisterGlobalAdmin()
+    {
+    	$client = static::createClient();
+    
+    	$crawler = $client->request('POST', '/admin/register/globalAdmin', ['email' => 'test4@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals($crawler->text(),"User created");
+    }
 }
