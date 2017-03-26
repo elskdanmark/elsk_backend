3c5cf1b21c5ba1907d5324ca5503f022dc6c5b05
Zeohye <Nilshenning@live.dk>
2017-03-26 17:16:42 +0200

    Added access denial test to UserController

--- src/Elsk/CoreBundle/Tests/Controller/UserControllerTest.php
+++ src/Elsk/CoreBundle/Tests/Controller/UserControllerTest.php
@@ -9,7 +9,6 @@ class UserControllerTest extends WebTestCase
     public function testRegisterRecipient()
     {
         $client = static::createClient();
-
         $crawler = $client->request('POST', '/register/recipient', ['email' => 'test@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
         $this->assertEquals($crawler->text(),"User created");
     }
@@ -17,24 +16,25 @@ class UserControllerTest extends WebTestCase
     public function testRegisterVolunteer()
     {
     	$client = static::createClient();
-    
     	$crawler = $client->request('POST', '/register/volunteer', ['email' => 'test2@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
     	$this->assertEquals($crawler->text(),"User created");
     }
-    
-    public function testRegisterLocalAdmin()
+
+    public function testRegisterLocalAdminDenial()
     {
     	$client = static::createClient();
-    
     	$crawler = $client->request('POST', '/admin/register/localAdmin', ['email' => 'test3@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
-    	$this->assertEquals($crawler->text(),"User created");
+    	$this->assertEquals(500, $client->getResponse()->getStatusCode());
+    	$this->assertTrue($crawler->filter('html:contains("The token storage contains no authentication token. One possible reason may be that there is no firewall configured for this URL. (500 Internal Server Error)")')->count() > 0);
     }
     
-    public function testRegisterGlobalAdmin()
+    public function testRegisterGlobalAdminDenial()
     {
     	$client = static::createClient();
-    
     	$crawler = $client->request('POST', '/admin/register/globalAdmin', ['email' => 'test4@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
-    	$this->assertEquals($crawler->text(),"User created");
+    	$this->assertEquals(500, $client->getResponse()->getStatusCode());
+    	$this->assertTrue($crawler->filter('html:contains("The token storage contains no authentication token. One possible reason may be that there is no firewall configured for this URL. (500 Internal Server Error)")')->count() > 0);
     }
+    
+    
 }
