52d8c5d135030b650dd75fda741bf58569611b17
Zeohye <Nilshenning@live.dk>
2017-03-26 18:29:45 +0200

    Added access allowed test to Local/Global admin create

--- src/Elsk/CoreBundle/Tests/Controller/UserControllerTest.php
+++ src/Elsk/CoreBundle/Tests/Controller/UserControllerTest.php
@@ -3,38 +3,65 @@
 namespace Elsk\CoreBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class UserControllerTest extends WebTestCase
 {
+	private $client = null;
+	public function setUp()
+	{
+		$this->client = static::createClient();
+	}
+	
     public function testRegisterRecipient()
     {
-        $client = static::createClient();
-        $crawler = $client->request('POST', '/register/recipient', ['email' => 'test@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+        $crawler = $this->client->request('POST', '/register/recipient', ['email' => 'test@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertEquals($crawler->text(),"User created");
     }
     
     public function testRegisterVolunteer()
     {
-    	$client = static::createClient();
-    	$crawler = $client->request('POST', '/register/volunteer', ['email' => 'test2@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$crawler = $this->client->request('POST', '/register/volunteer', ['email' => 'test2@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     	$this->assertEquals($crawler->text(),"User created");
     }
 
     public function testRegisterLocalAdminDenial()
     {
-    	$client = static::createClient();
-    	$crawler = $client->request('POST', '/admin/register/localAdmin', ['email' => 'test3@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
-    	$this->assertEquals(500, $client->getResponse()->getStatusCode());
-    	$this->assertTrue($crawler->filter('html:contains("The token storage contains no authentication token. One possible reason may be that there is no firewall configured for this URL. (500 Internal Server Error)")')->count() > 0);
+    	$crawler = $this->client->request('POST', '/admin/register/localAdmin', ['email' => 'test3@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals(401, $this->client->getResponse()->getStatusCode());
     }
     
     public function testRegisterGlobalAdminDenial()
     {
-    	$client = static::createClient();
-    	$crawler = $client->request('POST', '/admin/register/globalAdmin', ['email' => 'test4@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
-    	$this->assertEquals(500, $client->getResponse()->getStatusCode());
-    	$this->assertTrue($crawler->filter('html:contains("The token storage contains no authentication token. One possible reason may be that there is no firewall configured for this URL. (500 Internal Server Error)")')->count() > 0);
+    	$crawler = $this->client->request('POST', '/admin/register/globalAdmin', ['email' => 'test4@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals(401, $this->client->getResponse()->getStatusCode());
+    }
+    
+    public function testRegisterLocalAdmin()
+    {
+    	$this->superAdminLogin();
+    	$crawler = $this->client->request('POST', '/admin/register/localAdmin', ['email' => 'test3@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    	$this->assertEquals($crawler->text(),"User created");
+    }
+    
+    public function testRegisterGlobalAdmin()
+    {
+    	$this->superAdminLogin();
+    	$crawler = $this->client->request('POST', '/admin/register/globalAdmin', ['email' => 'test4@test.dk',"password"=>"test1234!","firstName"=>"bob","lastName"=>"bobson","phone"=>"12345678","elskCity"=>"Aarhus"]);
+    	$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    	$this->assertEquals($crawler->text(),"User created");
     }
     
     
+    private function superAdminLogin()
+    {
+    	$this->client = static::createClient(array(), array(
+    			'PHP_AUTH_USER' => 'superAdmin',
+    			'PHP_AUTH_PW'   => 'superAdminPass',
+    	));
+    }
 }
